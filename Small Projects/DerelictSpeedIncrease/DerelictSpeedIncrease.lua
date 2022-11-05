b43f4ad98a5703733b4df7378e578efe685bdb07
Jackty89 <90223197+Jackty89@users.noreply.github.com>
2022-11-05 19:46:15 +0100

    Add DerelictSpeedIncrease

---
+++ Small Projects/DerelictSpeedIncrease/DerelictSpeedIncrease.lua
@@ -0,0 +1,35 @@
+ModeName = "DerelictSpeedIncrease"
+Author = "Jackty89"
+
+SpeedMultiplier = "1"
+
+GroundWalkSpeed = "4.4" --these are their actual values
+GroundRunSpeed = "8" --these are their actual values
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"] = ModeName .. ".pak",
+    ["MOD_DESCRIPTION"] = ModeName,
+    ["MOD_AUTHOR"] = Author,
+    ["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCPLAYERGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"GroundWalkSpeedLowG", (GroundWalkSpeed * SpeedMultiplier)},
+                                {"GroundRunSpeedLowG", (GroundRunSpeed * SpeedMultiplier)}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
